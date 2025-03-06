c13afd5353fe176ea1e8f3c69372c35db7dcce24
pranav-kural <pranavkural042@gmail.com>
2025-03-06 18:49:18 -0500

    Updated chat header + chat area + chat input styling + content

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useWindowSize } from 'usehooks-ts';
+import { Geist_Mono } from 'next/font/google';
 
 import { ModelSelector } from '@/components/model-selector';
 import { SidebarToggle } from '@/components/sidebar-toggle';
@@ -12,6 +13,10 @@ import { useSidebar } from './ui/sidebar';
 import { memo } from 'react';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 import { VisibilityType, VisibilitySelector } from './visibility-selector';
+
+const geistMono = Geist_Mono({
+	subsets: ['latin'],
+});
 
 function PureChatHeader({
 	chatId,
@@ -25,59 +30,89 @@ function PureChatHeader({
 	isReadonly: boolean;
 }) {
 	const router = useRouter();
-	const { open } = useSidebar();
+	const { open, setOpenMobile } = useSidebar();
 
 	const { width: windowWidth } = useWindowSize();
 
 	return (
-		<header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
-			<SidebarToggle />
-
-			{(!open || windowWidth < 768) && (
-				<Tooltip>
-					<TooltipTrigger asChild>
-						<Button
-							variant="outline"
-							className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
-							onClick={() => {
-								router.push('/');
-								router.refresh();
-							}}
-						>
-							<PlusIcon />
-							<span className="md:sr-only">New Chat</span>
-						</Button>
-					</TooltipTrigger>
-					<TooltipContent>New Chat</TooltipContent>
-				</Tooltip>
-			)}
-
-			{!isReadonly && (
-				<ModelSelector
-					selectedModelId={selectedModelId}
-					className="order-1 md:order-2"
-				/>
-			)}
-
-			{!isReadonly && (
-				<VisibilitySelector
-					chatId={chatId}
-					selectedVisibilityType={selectedVisibilityType}
-					className="order-1 md:order-3"
-				/>
-			)}
-
-			<Button
-				className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
-				asChild
+		<header className="flex flex-col sticky top-0 bg-neutral-100 dark:bg-zinc-950 py-1.5 items-center px-2 md:px-2 gap-2">
+			<Link
+				href="/"
+				onClick={() => {
+					setOpenMobile(false);
+				}}
+				className="flex flex-col gap-1 w-full text-center lg:mr-[250px] xs:hidden px-2 hover:bg-muted rounded-md cursor-pointer"
 			>
-				<Link
-					href="https://github.com/pranav-kural/fragment-ai-assistant-web"
-					target="_noblank"
+				<span
+					className={`${geistMono.className} text-lg font-semibold px-2 hover:bg-muted`}
 				>
-					<GitIcon />
+					FRAGMENT AI Assistant
+				</span>
+			</Link>
+
+			<div className="xs:w-unset w-full flex sticky top-0 items-center gap-2">
+				<SidebarToggle />
+
+				{(!open || windowWidth < 768) && (
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								variant="outline"
+								className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-0"
+								onClick={() => {
+									router.push('/');
+									router.refresh();
+								}}
+							>
+								<PlusIcon />
+								<span className="md:sr-only">New Chat</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent>New Chat</TooltipContent>
+					</Tooltip>
+				)}
+
+				{!isReadonly && (
+					<ModelSelector
+						selectedModelId={selectedModelId}
+						className="order-1 md:order-2"
+					/>
+				)}
+
+				{!isReadonly && (
+					<VisibilitySelector
+						chatId={chatId}
+						selectedVisibilityType={selectedVisibilityType}
+						className="order-1 md:order-3"
+					/>
+				)}
+
+				<Link
+					href="/"
+					onClick={() => {
+						setOpenMobile(false);
+					}}
+					className="text-right px-2 w-full order-4 lg:text-center lg:block lg:mr-[250px] hidden xs:block rounded-md cursor-pointer"
+				>
+					<span
+						className={`${geistMono.className} text-lg font-semibold px-2 hover:bg-muted`}
+					>
+						FRAGMENT AI Assistant
+					</span>
 				</Link>
-			</Button>
+
+				<Button
+					className="bg-zinc-700 dark:bg-zinc-300 hover:bg-zinc-900 hover:shadow-md dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-5 md:ml-auto md:mr-5"
+					asChild
+				>
+					<Link
+						href="https://github.com/pranav-kural/fragment-ai-assistant-web"
+						target="_noblank"
+					>
+						<GitIcon />
+					</Link>
+				</Button>
+			</div>
 		</header>
 	);
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -13,111 +13,135 @@ import { Messages } from './messages';
 import { VisibilityType } from './visibility-selector';
 import { useArtifactSelector } from '@/hooks/use-artifact';
 import { toast } from 'sonner';
+import { Geist_Mono } from 'next/font/google';
+import Link from 'next/link';
+import { SquareArrowUpRightIcon } from 'lucide-react';
+
+const geistMono = Geist_Mono({
+	subsets: ['latin'],
+});
 
 export function Chat({
-  id,
-  initialMessages,
-  selectedChatModel,
-  selectedVisibilityType,
-  isReadonly,
+	id,
+	initialMessages,
+	selectedChatModel,
+	selectedVisibilityType,
+	isReadonly,
 }: {
-  id: string;
-  initialMessages: Array<Message>;
-  selectedChatModel: string;
-  selectedVisibilityType: VisibilityType;
-  isReadonly: boolean;
+	id: string;
+	initialMessages: Array<Message>;
+	selectedChatModel: string;
+	selectedVisibilityType: VisibilityType;
+	isReadonly: boolean;
 }) {
-  const { mutate } = useSWRConfig();
+	const { mutate } = useSWRConfig();
 
-  const {
-    messages,
-    setMessages,
-    handleSubmit,
-    input,
-    setInput,
-    append,
-    isLoading,
-    stop,
-    reload,
-  } = useChat({
-    id,
-    body: { id, selectedChatModel: selectedChatModel },
-    initialMessages,
-    experimental_throttle: 100,
-    sendExtraMessageFields: true,
-    generateId: generateUUID,
-    onFinish: () => {
-      mutate('/api/history');
-    },
-    onError: () => {
-      toast.error('An error occured, please try again!');
-    },
-  });
+	const {
+		messages,
+		setMessages,
+		handleSubmit,
+		input,
+		setInput,
+		append,
+		isLoading,
+		stop,
+		reload,
+	} = useChat({
+		id,
+		body: { id, selectedChatModel: selectedChatModel },
+		initialMessages,
+		experimental_throttle: 100,
+		sendExtraMessageFields: true,
+		generateId: generateUUID,
+		onFinish: () => {
+			mutate('/api/history');
+		},
+		onError: () => {
+			toast.error('An error occured, please try again!');
+		},
+	});
 
-  const { data: votes } = useSWR<Array<Vote>>(
-    `/api/vote?chatId=${id}`,
-    fetcher,
-  );
+	const { data: votes } = useSWR<Array<Vote>>(
+		`/api/vote?chatId=${id}`,
+		fetcher
+	);
 
-  const [attachments, setAttachments] = useState<Array<Attachment>>([]);
-  const isArtifactVisible = useArtifactSelector((state) => state.isVisible);
+	const [attachments, setAttachments] = useState<Array<Attachment>>([]);
+	const isArtifactVisible = useArtifactSelector((state) => state.isVisible);
 
-  return (
-    <>
-      <div className="flex flex-col min-w-0 h-dvh bg-background">
-        <ChatHeader
-          chatId={id}
-          selectedModelId={selectedChatModel}
-          selectedVisibilityType={selectedVisibilityType}
-          isReadonly={isReadonly}
-        />
+	return (
+		<>
+			<div className="flex flex-col min-w-0 h-dvh bg-neutral-100 dark:bg-zinc-950">
+				<ChatHeader
+					chatId={id}
+					selectedModelId={selectedChatModel}
+					selectedVisibilityType={selectedVisibilityType}
+					isReadonly={isReadonly}
+				/>
 
-        <Messages
-          chatId={id}
-          isLoading={isLoading}
-          votes={votes}
-          messages={messages}
-          setMessages={setMessages}
-          reload={reload}
-          isReadonly={isReadonly}
-          isArtifactVisible={isArtifactVisible}
-        />
+				<Messages
+					chatId={id}
+					isLoading={isLoading}
+					votes={votes}
+					messages={messages}
+					setMessages={setMessages}
+					reload={reload}
+					isReadonly={isReadonly}
+					isArtifactVisible={isArtifactVisible}
+				/>
 
-        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
-          {!isReadonly && (
-            <MultimodalInput
-              chatId={id}
-              input={input}
-              setInput={setInput}
-              handleSubmit={handleSubmit}
-              isLoading={isLoading}
-              stop={stop}
-              attachments={attachments}
-              setAttachments={setAttachments}
-              messages={messages}
-              setMessages={setMessages}
-              append={append}
-            />
-          )}
-        </form>
-      </div>
+				<form className="flex mx-auto px-4 pb-2 gap-2 w-full md:max-w-3xl">
+					{!isReadonly && (
+						<MultimodalInput
+							chatId={id}
+							input={input}
+							setInput={setInput}
+							handleSubmit={handleSubmit}
+							isLoading={isLoading}
+							stop={stop}
+							attachments={attachments}
+							setAttachments={setAttachments}
+							messages={messages}
+							setMessages={setMessages}
+							append={append}
+						/>
+					)}
+				</form>
 
-      <Artifact
-        chatId={id}
-        input={input}
-        setInput={setInput}
-        handleSubmit={handleSubmit}
-        isLoading={isLoading}
-        stop={stop}
-        attachments={attachments}
-        setAttachments={setAttachments}
-        append={append}
-        messages={messages}
-        setMessages={setMessages}
-        reload={reload}
-        votes={votes}
-        isReadonly={isReadonly}
-      />
-    </>
-  );
+				<p className="w-full text-center text-xs pb-2 md:pb-4">
+					This is a restricted chat. You will only be able to converse
+					about{' '}
+					<span className={`${geistMono.className} font-semibold`}>
+						FRAGMENT
+					</span>{' '}
+					. Learn more about this project{' '}
+					<Link
+						className="font-medium hover:text-blue-500 underline underline-offset-2"
+						href="https://www.pkural.ca/blog/posts/fragment/"
+						target="_blank"
+					>
+						here
+					</Link>
+					.
+				</p>
+			</div>
+
+			<Artifact
+				chatId={id}
+				input={input}
+				setInput={setInput}
+				handleSubmit={handleSubmit}
+				isLoading={isLoading}
+				stop={stop}
+				attachments={attachments}
+				setAttachments={setAttachments}
+				append={append}
+				messages={messages}
+				setMessages={setMessages}
+				reload={reload}
+				votes={votes}
+				isReadonly={isReadonly}
+			/>
+		</>
+	);
 }

e17f22ac7689641340dbf969446db6c7f28c1747
pranav-kural <pranavkural042@gmail.com>
2025-03-06 18:48:51 -0500

    Updates app sidebar styling

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,66 +2,81 @@
 
 import type { User } from 'next-auth';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Geist_Mono } from 'next/font/google';
+import { SquareArrowOutUpRight } from 'lucide-react';
 
 import { PlusIcon } from '@/components/icons';
 import { SidebarHistory } from '@/components/sidebar-history';
 import { SidebarUserNav } from '@/components/sidebar-user-nav';
 import { Button } from '@/components/ui/button';
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  useSidebar,
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	useSidebar,
 } from '@/components/ui/sidebar';
-import Link from 'next/link';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
-export function AppSidebar({ user }: { user: User | undefined }) {
-  const router = useRouter();
-  const { setOpenMobile } = useSidebar();
+const geistMono = Geist_Mono({
+	subsets: ['latin'],
+});
 
-  return (
-    <Sidebar className="group-data-[side=left]:border-r-0">
-      <SidebarHeader>
-        <SidebarMenu>
-          <div className="flex flex-row justify-between items-center">
-            <Link
-              href="/"
-              onClick={() => {
-                setOpenMobile(false);
-              }}
-              className="flex flex-row gap-3 items-center"
-            >
-              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Chatbot
-              </span>
-            </Link>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  type="button"
-                  className="p-2 h-fit"
-                  onClick={() => {
-                    setOpenMobile(false);
-                    router.push('/');
-                    router.refresh();
-                  }}
-                >
-                  <PlusIcon />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent align="end">New Chat</TooltipContent>
-            </Tooltip>
-          </div>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarHistory user={user} />
-      </SidebarContent>
-      <SidebarFooter>{user && <SidebarUserNav user={user} />}</SidebarFooter>
-    </Sidebar>
-  );
+export function AppSidebar({ user }: { user: User | undefined }) {
+	const router = useRouter();
+	const { setOpenMobile } = useSidebar();
+
+	return (
+		<Sidebar className="group-data-[side=left]:border-r-0">
+			<SidebarHeader>
+				<SidebarMenu>
+					<div className="flex flex-row justify-between items-center">
+						<div className="w-full flex flex-col gap-2">
+							<span className="text-md font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
+								History
+							</span>
+						</div>
+
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Button
+									variant="ghost"
+									type="button"
+									className="p-2 h-fit"
+									onClick={() => {
+										setOpenMobile(false);
+										router.push('/');
+										router.refresh();
+									}}
+								>
+									<PlusIcon />
+								</Button>
+							</TooltipTrigger>
+							<TooltipContent align="end">
+								New Chat
+							</TooltipContent>
+						</Tooltip>
+					</div>
+					<Link
+						href="https://fragment.dev/docs"
+						className="flex flex-row gap-1 px-2 hover:text-emerald-700 cursor-pointer"
+						target="_blank"
+					>
+						<span className={`${geistMono.className} text-sm`}>
+							FRAGMENT docs
+						</span>
+						<SquareArrowOutUpRight size={16} />
+					</Link>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarHistory user={user} />
+			</SidebarContent>
+			<SidebarFooter>
+				{user && <SidebarUserNav user={user} />}
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
